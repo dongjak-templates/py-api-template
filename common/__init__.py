89f6f74c9f21b06ec05e16675b0d4471e865cdf4
cruld <cruldra@gmail.com>
2025-02-26 04:38:26 +0800

    🗑️ refactor(common/sms.py): 删除不再使用的短信服务类及相关代码

--- common/__init__.py
+++ common/__init__.py
@@ -3,13 +3,8 @@ from .configs import settings
 from .jwt import create_jwt_token
 from .log import payment_logger, agents_logger, chat_logger
 from .models import *
-#{{#if (not includeAliSms)}}
-#  from .sms import sms_service
-#{{/if}}
-
 from .redis import redis_client
 from .routes import router
-
 
 __all__ = [
     "Error",
@@ -19,9 +14,6 @@ __all__ = [
     "agents_logger",
     "settings",
     "payment_logger",
-#{{#if (not includeAliSms)}}
-#  "sms_service",
-#{{/if}}
     "redis_client",
     "chat_logger",
     "DataPage",
